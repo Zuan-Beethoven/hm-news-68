ad5d58e0e08fbf466d612a5b750ea231e986e4d5
祖安贝多芬 <1426153262@qq.com>
2020-08-27 16:34:01 +0800

    配置了在项目中的vant

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 导入通用样式
+import './style/bess.less'
+import './style/iconfont.css'
+// 全局导入'vant'
+// import Vant from 'vant'
+// import 'vant/lib/index.css'
+// Vue.use(Vant)
+// 自动按需加载
+import { Button } from 'vant'
+// import Button from 'vant/lib/button'
+// import 'vant/lib/button/style'
+Vue.use(Button)
 Vue.config.productionTip = false
 
 new Vue({
